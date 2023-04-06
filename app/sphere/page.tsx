eef2e0a9e74630637580726f1bba173d5b71858f
Kim Yoo <kimyoo04eco@naver.com>
2023-04-06 22:51:29 +0900

    Update three.js setsize width and height when resize

--- app/sphere/page.tsx
+++ app/sphere/page.tsx
@@ -7,39 +7,51 @@ export default function Sphere(): JSX.Element {
   const sphereRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    // Scene, camera, renderer를 생성합니다.
+    const renderer = new THREE.WebGLRenderer();
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
+      75, // 시야각도
+      window.innerWidth / window.innerHeight, // aspect
+      0.1, // near: 최소 z
+      100 // far: 최대 z
     );
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    sphereRef.current?.appendChild(renderer.domElement);
+    const light = new THREE.PointLight(0xffffff, 1, 100);
+    const geometry = new THREE.SphereGeometry(1, 64, 64); // 반지름, 폭세그먼트 수, 높이세그먼트 수
+    const material = new THREE.MeshStandardMaterial({ color: "#d3de0b" });
+    const Mesh = new THREE.Mesh(geometry, material);
 
-    // Sphere를 생성합니다.
-    const geometry = new THREE.SphereGeometry(1, 32, 32);
-    const material = new THREE.MeshBasicMaterial({
-      color: 0xff0000,
-      wireframe: true,
-    });
-    const sphere = new THREE.Mesh(geometry, material);
-    scene.add(sphere);
+    camera.position.z = 4; // 카메라 깊이
+    light.position.set(0, 10, 10); // x, y, z
 
-    // 카메라를 설정합니다.
-    camera.position.z = 5;
+    scene.add(camera);
+    scene.add(light);
+    scene.add(Mesh);
 
-    // 애니메이션 루프를 생성합니다.
+    function handleResize() {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+
+      renderer.setSize(width, height);
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+    }
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    sphereRef.current?.appendChild(renderer.domElement); // div 태그에 할당
+
+    // 애니메이션 루프를 생성
     function animate() {
       requestAnimationFrame(animate);
-      sphere.rotation.x += 0.01;
-      sphere.rotation.y += 0.01;
       renderer.render(scene, camera);
     }
     animate();
+
+    // 컴포넌트 언마운트 시 이벤트 제거
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return <div ref={sphereRef} />;
 }
+
+// geometry > material > Mesh(geometry, material) > scene.add(mesh)
+// > WebGLRenderer > renderer.setSize() > renderer.render(scene, camera)
