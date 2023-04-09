4b7816b02ce9e6c6cbb27871002c0d72905607dd
Kim Yoo <kimyoo04eco@naver.com>
2023-04-09 23:39:46 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+};
+const withTM = require("next-transpile-modules")(["three"]);
 
-module.exports = nextConfig
+module.exports = withTM();
+module.exports = nextConfig;
